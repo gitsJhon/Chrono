2f301c7493dbccde548700a7981ad91e9bbe6a4e
gitsJhon <sebas2002jhon@gmail.com>
2025-05-21 08:21:51 -0500

    modifico los datos enviados, en un diccionario, añado el consumo de recursos

--- backend/fun/server.py
+++ backend/fun/server.py
@@ -2,11 +2,29 @@ import asyncio
 import json
 import websockets
 from app_time_tracker import get_apps_with_time_and_category
+from recursos import apps_consumo
 
 async def apps_handler(websocket):
     while True:
-        apps = get_apps_with_time_and_category()
-        await websocket.send(json.dumps(apps))
+        apps_tiempo = get_apps_with_time_and_category()  # lista de apps
+        consumo_dict = apps_consumo()  # dict de consumo por app
+
+        # convertir consumo_dict a lista como espera tu JS
+        apps_consumo_list = []
+        for name, valores in consumo_dict.items():
+            apps_consumo_list.append({
+                'name': name,
+                'gpu': valores['gpu_percent'] if valores['gpu_percent'] is not None else 0,
+                'cpu': valores['cpu_percent'],
+                'ram': valores['memory_mb']
+            })
+
+        paquete = {
+            "apps_tiempo": apps_tiempo,
+            "apps_consumo": apps_consumo_list
+        }
+
+        await websocket.send(json.dumps(paquete))
         await asyncio.sleep(2)
 
 async def main():
